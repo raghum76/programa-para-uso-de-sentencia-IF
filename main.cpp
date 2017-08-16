c7f592d96870589ac687cfebc3c5331588649273
raghum76 <raghum76@ucol.mx>
2017-08-15 22:11:06 -0500

    Update main.cpp
    Solo cuida la identación de tus codigos y las llaves de cierre

--- main.cpp
+++ main.cpp
@@ -9,23 +9,28 @@ int main()
     if (sueldo >= 0 && sueldo <=1000)
     {
         sueldo = sueldo * 1.18 ;
-        cout << "tu nuevo sueldo es : " << sueldo ; }
+        cout << "tu nuevo sueldo es : " << sueldo ; 
+    }
     else if (sueldo >=1001 && sueldo <=1100)
     {
         sueldo = sueldo * 1.15;
-        cout << "tu nuevo sueldo es : " << sueldo ;}
+        cout << "tu nuevo sueldo es : " << sueldo ;
+    }
             else if ( sueldo >=1101 && sueldo <=1200)
             {
                 sueldo = sueldo * 1.12;
-                cout <<" tu nuevo sueldo es : " << sueldo ;}
+                cout <<" tu nuevo sueldo es : " << sueldo ;
+            }
                     else if ( sueldo >=1201 && sueldo <=1300)
                     {
                         sueldo = sueldo * 1.10;
-                        cout <<" tu nuevo sueldo es : " << sueldo ;}
+                        cout <<" tu nuevo sueldo es : " << sueldo ;
+                    }
                         else
                         {
                             sueldo = sueldo * 1.08 ;
-                            cout << "tu nuevo sueldo es : " << sueldo ; }
-
+                            cout << "tu nuevo sueldo es : " << sueldo ; 
+                        }
+//No debes poner las llaves de cierre al final de la instrucción, sera complicado revisar despues.
     return 0 ;
 }
